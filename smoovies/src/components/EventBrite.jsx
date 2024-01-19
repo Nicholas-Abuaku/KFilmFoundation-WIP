bd190b98ed635d18bbc27ebcaee014bb0973cf06
Nicholas Abuaku <abuaku1400@gmail.com>
2024-01-19 13:40:08 +0000

    Makes request to eventbrite and stores relevant event info

--- smoovies/src/components/EventBrite.jsx
+++ smoovies/src/components/EventBrite.jsx
@@ -1,23 +1,43 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
+import { MCard } from './MCard';
+import { LatestScreeningCard } from './LatestScreeningCard';
 export const EventBrite = () => {
-    const API_KEY = 'O6A4J6574FEEZXGNQGQP';
+    const [name,setName] = useState([]);
+    const [desc,setDesc] = useState([]);
+    const [date,setDate] = useState([]);
+    const [stuff,setStuff] = useState([]);
+    const API_KEY = '7CHF5MRXCX4LJX2ENE2Q';
     const headers = {
         'Authorization': `Bearer ${API_KEY}`,
         'Content-Type': 'application/json'
     };
-
+// "335808768129"  eventbriteapi.com/v3/organizations/organizationid/events/335808768129
     const getStuff = async ()=>{
         try{
-            axios.get('https://www.eventbriteapi.com/v3/users/me/?token=O6A4J6574FEEZXGNQGQP',{headers})
+            axios.get('https://www.eventbriteapi.com/v3/events/803841630957/',{headers})
             .then(response=>{
-                console.log(response.data);
+                setStuff(response.data);
+                var dateFormatted = response.data.start.local;
+                setDate(response.data.start.local)
+                setName(response.data.name.text);
+                setDesc(response.data.description.text);
             })
         }catch(err){
             console.log(err)
         }
     }
+
+    useEffect(()=>{
+        getStuff()
+        
+    },[])
   return (
-    getStuff()
+    <>
+    {console.log(name, desc, date)}
+
+    <LatestScreeningCard/>
+    </>
+    
   )
 }
