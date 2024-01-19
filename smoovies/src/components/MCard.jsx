171c2287cb2eb856b808ba891281f99c3567aeab
Nicholas Abuaku <abuaku1400@gmail.com>
2024-01-19 13:39:35 +0000

    Changed styling on movie cards. Now scrollable when description too long

--- smoovies/src/components/MCard.jsx
+++ smoovies/src/components/MCard.jsx
@@ -3,13 +3,15 @@ import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
-import { Box, IconButton, Stack, Typography, Icon } from '@mui/material';
+import { Box, IconButton, Stack, Typography, Icon, CardActionArea } from '@mui/material';
 import PGIcon from '../assets/icons/bbfc-pg-certificate-uk-seeklogo.svg'
 export const MCard = (props) => {
   return (
+    <CardActionArea>
     <Card sx={{ width:'29.59vw', height:'29vh', display:'flex', backgroundColor:'#F0F0ED ', color:'black'}} elevation={10}> 
-        <CardMedia image={props.img} title={props.title} sx={{ minWidth:'8.5vw', minHeight:'27.51vh', objectFit:'cover', marginRight:'5px' }}/>
-        <Box sx={{ wordWrap:'break-word'}}>
+   
+        <CardMedia component='img' image={props.img} title={props.title} sx={{ maxWidth:'8.5vw', maxHeight:'27.51vh', objectFit:'cover', marginRight:'5px' }}/>
+        <Box sx={{ wordWrap:'break-word', overflow:'auto'}}>
             <Stack direction={'row'} spacing={10}>
             <Typography fontWeight={'bold'} variant='h5'>{props.title}</Typography>
             <Icon>
@@ -21,6 +23,8 @@ export const MCard = (props) => {
             {/* <hr/> */}
             <Typography sx={{marginTop:'10px'}}>{props.description}</Typography>
         </Box>
+       
     </Card>
+    </CardActionArea>
   )
 }
