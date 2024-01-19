fe7591ebd1f50eaf3469b7edc321f553d897e8c0
Nicholas Abuaku <abuaku1400@gmail.com>
2024-01-19 13:37:58 +0000

    Updated card grid with relevant information

--- smoovies/src/components/CardGrid.jsx
+++ smoovies/src/components/CardGrid.jsx
@@ -8,51 +8,51 @@ export const CardGrid = () => {
     const movies = [
         {
             id:1,
-            title:'Caitlin Gorgeous',
-            img: '',
-            runningTime:'1',
-            date:'Saturday 13th January 2024',
-            description:'Something amazing Caitlin jus whipped up'
+            title:'My Old School (Documentary)',
+            img: 'https://m.media-amazon.com/images/M/MV5BZmFmM2ZkYzAtNmEzZC00MTEyLTg3ZTgtZGM4MTYwYjc2MDNkXkEyXkFqcGdeQXVyMTg2NjYzOA@@._V1_FMjpg_UY519_.jpg',
+            runningTime:'104',
+            date:'Thursday 25th January',
+            description:'In 1993, a boy named Brandon Lee enrolls at the Bearsden Academy secondary school in Glasgow, Scotland. Over time, it is revealed that Brandon Lee is not who he seems.'
         },
         {
             id:2,
-            title:'The Boy and The Heron',
-            img:"https://m.media-amazon.com/images/M/MV5BZjZkNThjNTMtOGU0Ni00ZDliLThmNGUtZmMxNWQ3YzAxZTQ1XkEyXkFqcGdeQXVyMTUzMTg2ODkz._V1_FMjpg_UX1000_.jpg",
-            runningTime:'124',
-            date:'Saturday 14th July 2024',
-            description:"Mahito, a young 12-year-old boy, struggles to settle in a new town after his mother's death. However, when a talking heron informs Mahito that his mother is still alive, he enters an abandoned tower in search of her, which takes him to another world."
+            title:'Picnic at Hanging Rock',
+            img:"https://m.media-amazon.com/images/M/MV5BNzk4NTljZDEtNDk0Ny00ODkxLWExMzItODVlODc4MDUxN2JkXkEyXkFqcGdeQXVyMTMxMTY0OTQ@._V1_.jpg",
+            runningTime:'115',
+            date:'Friday 26th January 2pm',
+            description:"Appleyard College, a girls' private school, plans a summer picnic to Hanging Rock in Victoria. However, things turn bad when a few students and a teacher go missing at the picnic."
         },
         {
             id:3,
-            title:'The Holdovers',
-            img:"https://m.media-amazon.com/images/M/MV5BNDc2MzNkMjMtZDY5NC00NmQ0LWI1NjctZjRhNWIzZjc4MGRiXkEyXkFqcGdeQXVyMjkwOTAyMDU@._V1_.jpg",
-            runningTime:'150',
-            date:'Saturday 15th July 2024',
-            description:"A curmudgeonly instructor at a New England prep school remains on campus during Christmas break to babysit a handful of students with nowhere to go. He soon forms an unlikely bond with a brainy but damaged troublemaker, and with the school's head cook, a woman who just lost a son in the Vietnam War."
+            title:'The Caastle',
+            img:"https://m.media-amazon.com/images/M/MV5BMDM4MDFiMjEtOWFhOC00M2RjLWE2NDItZGZlZjVhOGVjZWExXkEyXkFqcGdeQXVyNjMwMjk0MTQ@._V1_.jpg",
+            runningTime:'85',
+            date:'Friday 26th January 7pm',
+            description:"A Melbourne family loves its home which is located next to the Melbourne airport. When government and airport authorities force them to vacate the house, they decide to fight for their beloved home."
         },
         {
             id:4,
-            title:'Poor Things',
-            img:"https://m.media-amazon.com/images/M/MV5BNGIyYWMzNjktNDE3MC00YWQyLWEyMmEtN2ZmNzZhZDk3NGJlXkEyXkFqcGdeQXVyMTUzMTg2ODkz._V1_.jpg",
-            runningTime:'141',
-            date:'Saturday 15th July 2024',
-            description:"Brought back to life by an unorthodox scientist, a young woman runs off with a lawyer on a whirlwind adventure across the continents. Free from the prejudices of her times, she grows steadfast in her purpose to stand for equality and liberation."
+            title:'A Good Person',
+            img:"https://m.media-amazon.com/images/M/MV5BYzBkZGYyM2QtNzgwYy00NmZjLTk2ZTItYzY3ZTdjY2M0YmQwXkEyXkFqcGdeQXVyMjkwOTAyMDU@._V1_.jpg",
+            runningTime:'129',
+            date:'Wednesday 31st 2pm',
+            description:"Allison is a young woman with a wonderful fiance, a blossoming career, and supportive family and friends. However, her world crumbles in the blink of an eye when she survives an unimaginable tragedy, emerging from recovery with an opioid addiction and unresolved grief. In the following years, she forms an unlikely friendship with her would-be father-in-law that gives her a fighting chance to put her life back together and move forward."
         },
         {
             id:5,
-            title:'The Holdovers',
-            img:"https://m.media-amazon.com/images/M/MV5BNDc2MzNkMjMtZDY5NC00NmQ0LWI1NjctZjRhNWIzZjc4MGRiXkEyXkFqcGdeQXVyMjkwOTAyMDU@._V1_.jpg",
-            runningTime:'150',
-            date:'Saturday 15th July 2024',
-            description:"A curmudgeonly instructor at a New England prep school remains on campus during Christmas break to babysit a handful of students with nowhere to go. He soon forms an unlikely bond with a brainy but damaged troublemaker, and with the school's head cook, a woman who just lost a son in the Vietnam War."
+            title:'Finite: The Climate Of Change',
+            img:"https://m.media-amazon.com/images/M/MV5BMDdkNWRjZTUtODE5NS00NmZjLTk3NmUtODIzNDI1ZmUxMGY5XkEyXkFqcGdeQXVyMTYyMzQ5NTUy._V1_.jpg",
+            runningTime:'99',
+            date:'Thursday 1st Feb',
+            description:"Concerned citizens in Germany and England put their bodies on the line to save an ancient forest from Europe's largest coal mine."
         },
         {
             id:6,
-            title:'The Holdovers',
-            img:"https://m.media-amazon.com/images/M/MV5BNDc2MzNkMjMtZDY5NC00NmQ0LWI1NjctZjRhNWIzZjc4MGRiXkEyXkFqcGdeQXVyMjkwOTAyMDU@._V1_.jpg",
-            runningTime:'150',
-            date:'Saturday 15th July 2024',
-            description:"A curmudgeonly instructor at a New England prep school remains on campus during Christmas break to babysit a handful of students with nowhere to go. He soon forms an unlikely bond with a brainy but damaged troublemaker, and with the school's head cook, a woman who just lost a son in the Vietnam War."
+            title:'The Servant',
+            img:"https://m.media-amazon.com/images/M/MV5BZmM5ODI3ODItYmJiNC00YzAzLTk4M2QtNjViMjM3MDE4YTZiXkEyXkFqcGdeQXVyMTMxMTY0OTQ@._V1_.jpg",
+            runningTime:'116',
+            date:'Wednesday 7th Feb 2pm',
+            description:"Upper-class Tony hires servant Hugo Barrett, who turns out to have a hidden agenda."
         },
         
 
