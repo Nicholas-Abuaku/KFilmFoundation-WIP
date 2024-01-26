13d3a1a6a1d3432b97def1360ea18c289b8a3277
Nicholas Abuaku <abuaku1400@gmail.com>
2024-01-26 08:06:49 +0000

    Navigation drawer useable on smaller devices

--- smoovies/src/components/Navigation2.jsx
+++ smoovies/src/components/Navigation2.jsx
@@ -1,10 +1,13 @@
-import React from 'react'
-import {AppBar, Toolbar, IconButton, Button, Typography, Stack} from "@mui/material";
+import React, { useState } from 'react'
+import {AppBar, Toolbar, IconButton, Button, Typography, Stack, CssBaseline, useTheme, useMediaQuery} from "@mui/material";
 import TheaterComedyIcon from '@mui/icons-material/TheaterComedy';
 import KentFilmLogo from '../assets/icons/Untitled_Artwork.jpg'
-import App from '../App';
+import { NavDrawer } from './NavDrawer';
 import { Link } from 'react-router-dom';
 export const Navigation2 = () => {
+  const [openDrawer, setOpenDrawer] = useState(false);
+  const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.down("md"));
   return (
     <AppBar position='static' elevation={0}>
         <Toolbar sx={{ backgroundColor:'#339465', width:'100%'}} >
@@ -12,14 +15,17 @@ export const Navigation2 = () => {
                 <img src={KentFilmLogo} width={80} height={80}/>
             </IconButton>
             <Typography sx={{ flexGrow: '1', color:'white', fontWeight: 'bold' }} variant='h5'> Kent Film Foundation </Typography>
-            <Stack direction={'row'} spacing={2}>
-            <Button variant="text" color='inherit' component={Link} to={'/'}>Home</Button>
-                <Button variant="text" color='inherit' component={Link} to={'/film-clubs'}>Film Clubs</Button>
-                <Button variant='text' color='inherit' component={Link} to={'/press'}>Press</Button>
-                <Button variant='text' color='inherit' component={Link} to={'/about'}>About</Button>
-                <Button variant='text' color='inherit' component={Link} to={'/contact'}>Contact Us</Button>
-                
-            </Stack>
+            {isMobile ? (<NavDrawer/>):(
+                  <Stack direction={'row'} spacing={2}>
+                  <Button variant="text" color='inherit' component={Link} to={'/'}>Home</Button>
+                      <Button variant="text" color='inherit' component={Link} to={'/film-clubs'}>Film Clubs</Button>
+                      <Button variant='text' color='inherit' component={Link} to={'/press'}>Press</Button>
+                      <Button variant='text' color='inherit' component={Link} to={'/about'}>About</Button>
+                      <Button variant='text' color='inherit' component={Link} to={'/contact'}>Contact Us</Button>
+                      
+                  </Stack>
+            )}
+        
         </Toolbar>
     </AppBar>
   )
