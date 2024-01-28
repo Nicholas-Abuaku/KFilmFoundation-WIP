74c37d4206d0774e91e1e54e5ccc8ea9123f5fb3
Nicholas Abuaku <abuaku1400@gmail.com>
2024-01-28 10:08:42 +0000

    Removed unused imports and made /about route go to edit latest screening for testing purposes

--- smoovies/src/App.jsx
+++ smoovies/src/App.jsx
@@ -2,18 +2,15 @@
 
 import 'bootstrap/dist/css/bootstrap.css';
 import './App.css'
-
-
 import { Footer } from "./components/Footer";
 import { Home } from "./pages/Home";
 import { Routes, Route } from "react-router-dom";
 import {Contact} from './pages/Contact.jsx'
 import { FilmClub } from "./pages/FlimClub.jsx";
-
 import { Navigation2 } from "./components/Navigation2.jsx";
-
 import { About } from "./pages/About.jsx";
 import { Press } from "./pages/Press.jsx";
+import { EditLatestScreening } from './pages/back-end/EditLatestScreening.jsx';
 function App() {
 
   return (
@@ -24,7 +21,7 @@ function App() {
       <Route path="/contact" element={<Contact/>}/>
       <Route path="/film-clubs" element={<FilmClub/>}/>
       <Route path="/press" element={<Press/>}/>
-      <Route path="/about" element={<About/>}/>
+      <Route path="/about" element={<EditLatestScreening/>}/>
     </Routes>
     <Footer/>
     </>
