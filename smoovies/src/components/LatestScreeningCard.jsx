bc2dbcd168cb6b6dbcaedd44198011f13f866ef9
Nicholas Abuaku <abuaku1400@gmail.com>
2024-01-28 10:08:59 +0000

    imported link for tickets section

--- smoovies/src/components/LatestScreeningCard.jsx
+++ smoovies/src/components/LatestScreeningCard.jsx
@@ -5,6 +5,7 @@ import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import { Box, CardActionArea, CardHeader, Typography, useTheme, useMediaQuery, createTheme} from '@mui/material';
 import { ThemeProvider } from '@emotion/react';
+import { Link } from 'react-router-dom';
 export const LatestScreeningCard = (props) => {
     const theme = useTheme();
     const textTheme = createTheme();
@@ -50,13 +51,13 @@ export const LatestScreeningCard = (props) => {
     ):(
     <CardActionArea>
     <Card sx={{ display:'flex'}} width={'900px'} height={'63.49vh'}>
-      <Box>
+      <Box  maxHeight={'500px'} maxWidth={'974px'}>
     <CardMedia component={'img'}  height={'100%'} src={props.img} sx={{ objectFit:'fill', marginRight:'0px', width:'100%' }}/>
     </Box>
     <CardContent sx={{ backgroundColor:'#1A1A1A', color:'white', wordBreak:'break-word', width:'50%' }}>
         <Typography textAlign={'center'} variant='h1'>{props.title}</Typography>
         <Typography color={'white'}  paddingBottom={4} textAlign={'center'} variant='h4'>{props.date}</Typography>
-        <Typography color={'red'}  paddingBottom={4} textAlign={'center'} variant='h5'>Tickets</Typography>
+        <Typography color={'red'}  paddingBottom={4} textAlign={'center'} paddingLeft={'420px'} variant='h5' component={Link} to={props.ticketLink}>Tickets</Typography>
     </CardContent>
 </Card>
 </CardActionArea>
