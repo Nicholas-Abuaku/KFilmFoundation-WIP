a85558ddc6128dec0a0a443b149fcaeef5f19a85
Nicholas Abuaku <abuaku1400@gmail.com>
2024-01-29 16:49:02 +0000

    Dashboard page

--- smoovies/src/pages/back-end/Dashboard.jsx
+++ smoovies/src/pages/back-end/Dashboard.jsx
@@ -1,7 +1,18 @@
 import React from 'react'
+import { AdminNav } from '../../components/back-end/AdminNav'
+import { Grid, Typography} from '@mui/material'
+import { ButtonGrid } from '../../components/back-end/ButtonGrid'
+import { DashboardAnalytics } from '../../components/back-end/DashboardAnalytics'
 
 export const Dashboard = () => {
   return (
-   <h1>Dashboard</h1>
+    <Grid container justifyContent={'center'} alignItems={'center'} >
+      <Grid item xs={12}>
+        <ButtonGrid/>
+      </Grid>
+      <Grid item xs={12}>
+      <DashboardAnalytics/>
+      </Grid>
+    </Grid>
   )
 }
