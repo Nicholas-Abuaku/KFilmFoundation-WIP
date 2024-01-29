5cac2d487b9025ef4d65192aa31b288aafd18a0a
Nicholas Abuaku <abuaku1400@gmail.com>
2024-01-29 16:44:36 +0000

    About route goes to dashboard

--- smoovies/src/App.jsx
+++ smoovies/src/App.jsx
@@ -10,7 +10,8 @@ import { FilmClub } from "./pages/FlimClub.jsx";
 import { Navigation2 } from "./components/Navigation2.jsx";
 import { About } from "./pages/About.jsx";
 import { Press } from "./pages/Press.jsx";
-import { EditLatestScreening } from './pages/back-end/EditLatestScreening.jsx';
+import { EditLatestScreening } from './components/back-end/EditLatestScreening.jsx';
+import { Dashboard } from './pages/back-end/Dashboard.jsx';
 function App() {
 
   return (
@@ -21,7 +22,7 @@ function App() {
       <Route path="/contact" element={<Contact/>}/>
       <Route path="/film-clubs" element={<FilmClub/>}/>
       <Route path="/press" element={<Press/>}/>
-      <Route path="/about" element={<EditLatestScreening/>}/>
+      <Route path="/about" element={<Dashboard/>}/>
     </Routes>
     <Footer/>
     </>
