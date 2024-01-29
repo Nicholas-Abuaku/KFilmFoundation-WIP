df4dbaf203606930ab40bdf63a3076e483bb5d42
Nicholas Abuaku <abuaku1400@gmail.com>
2024-01-29 16:46:12 +0000

    Unsure what to do with this. Need to take up space on dashboard

---
+++ smoovies/src/components/back-end/DashboardAnalytics.jsx
@@ -0,0 +1,19 @@
+import { Box, Card, Paper } from '@mui/material'
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import React, { useState, useEffect } from 'react'
+import axios from 'axios';
+export const DashboardAnalytics = () => {
+
+
+
+  return (
+    <Box width={'100%'} height={400} sx={{ backgroundColor:'red' }}>
+
+    </Box>
+  )
+}
