d2054be336a706aa0c675419487e0995d7cae060
Nicholas Abuaku <abuaku1400@gmail.com>
2024-02-01 14:50:02 +0000

    Latest Sceening page

---
+++ smoovies/src/pages/back-end/LatestScreeningPage.jsx
@@ -0,0 +1,19 @@
+import { Grid, Typography } from '@mui/material'
+import React from 'react'
+import { EditLatestScreening } from '../../components/back-end/EditLatestScreening'
+
+export const LatestScreeningPage = () => {
+  return (
+   <Grid container>
+    <Grid item xs={12}>
+        <Typography variant='h3' textAlign={'center'}> Edit Latest Screening</Typography>
+    </Grid>
+    <Grid item xs={12}>
+        <Typography variant='h3'>Preview</Typography>
+    </Grid>
+    <Grid item xs={12}>
+        <EditLatestScreening/>
+    </Grid>
+   </Grid>
+  )
+}
