a258b3de1db61468e5d884e69dd38e0b47596a47
Nicholas Abuaku <abuaku1400@gmail.com>
2024-02-01 14:50:54 +0000

    Buton grid changed from stack to grid

--- smoovies/src/components/back-end/ButtonGrid.jsx
+++ smoovies/src/components/back-end/ButtonGrid.jsx
@@ -1,14 +1,28 @@
 import React from 'react'
-import { Stack } from '@mui/material';
+import { Grid, Stack, Paper } from '@mui/material';
 import { DashboardFeaturedButton } from './DashboardFeaturedButton';
 import { DashboardManageButton } from './DashboardManageButton';
 import { DashboardManageNewsButton } from './DashboardManageNewsButton';
 export const ButtonGrid = () => {
   return (
-    <Stack direction={'row-reverse'} spacing={2} marginTop={3}>
+    // <Stack direction={'row-reverse'} spacing={2} marginTop={3}>
+    //     <DashboardFeaturedButton/>
+    //     <DashboardManageButton/>
+    //     <DashboardManageNewsButton/>
+    // </Stack>
+    <Grid container spacing={2} justifyContent={'center'} alignItems={'center'} marginTop={'30%'} marginLeft={'120%'}>
+      <Grid item xs={8}>
         <DashboardFeaturedButton/>
+      </Grid>
+      <Grid item xs={4}>
         <DashboardManageButton/>
+      </Grid>
+      <Grid item xs={8}>
         <DashboardManageNewsButton/>
-    </Stack>
+      </Grid>
+      <Grid item xs={4}>
+        <DashboardManageNewsButton/>
+      </Grid>
+    </Grid>
   )
 }
