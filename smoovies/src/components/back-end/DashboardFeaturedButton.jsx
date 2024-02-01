335b1322110c5cb2dbebfefa192acba3972daf40
Nicholas Abuaku <abuaku1400@gmail.com>
2024-02-01 14:51:46 +0000

    Dashboard featured button now takes to edit featured page

--- smoovies/src/components/back-end/DashboardFeaturedButton.jsx
+++ smoovies/src/components/back-end/DashboardFeaturedButton.jsx
@@ -1,10 +1,11 @@
 import { Card, CardMedia, CardContent, Stack, Typography, Icon, CardActionArea } from '@mui/material'
 import React from 'react'
 import ManageHistoryIcon from '@mui/icons-material/ManageHistory';
+import { Link } from 'react-router-dom';
 export const DashboardFeaturedButton = (props) => {
   return (
-    <CardActionArea sx={{ width:'300px', height:'140px'}}>
-    <Card sx={{ width:'300px', height:'140px', background:'linear-gradient(90deg, rgba(0,36,4,1) 0%, rgba(43,125,74,1) 50%)', color:'white' }}>
+    <CardActionArea sx={{ width:'300px', height:'140px'}} component={Link} to={'/dashboard/edit-latest-screening'}>
+    <Card sx={{ width:'300px', height:'140px', background:'linear-gradient(62deg, #FBAB7E 0%, #F7CE68 100%);', color:'white' }}>
         <CardContent> 
             <Stack direction={'row'} spacing={1}>
             <Typography variant='h4'>Manage Featured Content</Typography>
