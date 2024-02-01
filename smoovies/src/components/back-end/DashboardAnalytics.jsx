36fe1c162e8b04bdc9805f17a49e50811df7c13a
Nicholas Abuaku <abuaku1400@gmail.com>
2024-02-01 14:51:07 +0000

    Dashboard analytics table

--- smoovies/src/components/back-end/DashboardAnalytics.jsx
+++ smoovies/src/components/back-end/DashboardAnalytics.jsx
@@ -1,4 +1,4 @@
-import { Box, Card, Paper } from '@mui/material'
+import { Box, Card, Paper, Typography } from '@mui/material'
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
 import TableCell from '@mui/material/TableCell';
@@ -8,12 +8,50 @@ import TableRow from '@mui/material/TableRow';
 import React, { useState, useEffect } from 'react'
 import axios from 'axios';
 export const DashboardAnalytics = () => {
+  const API_KEY = import.meta.env.VITE_Event_API_KEY;
+  const [sales,setSales] = useState([])
+  const headers = {
+      'Authorization': `Bearer ${API_KEY}`,
+      'Content-Type': 'application/json'
+  };
 
 
+  const fetchAllEvents = async () => {
+      try {
+          const response = await axios.get("  https://www.eventbriteapi.com/v3/organizations/335808768129/events/ ", { headers });
+          setSales(response.data.events);
+          console.log(sales)
+      } catch (err) {
+          console.log(err);
+      }
+  }
+
+  useEffect(() => {
+      fetchAllEvents();
+  }, [])
 
   return (
-    <Box width={'100%'} height={400} sx={{ backgroundColor:'red' }}>
-
-    </Box>
+    <TableContainer component={Paper} sx={{ width:'100%', maxHeight:'50%' }} elevation={24}>
+        <Table stickyHeader>
+            <TableHead>
+                <TableRow>
+                <TableCell>Event ID</TableCell>
+                    <TableCell>Name</TableCell>
+                    <TableCell>Date</TableCell>
+                    <TableCell>Capacity</TableCell>
+                </TableRow>
+            </TableHead>
+            <TableBody>
+                {sales.map((event)=>(
+                    <TableRow key={event.id}>
+                        <TableCell>{event.id}</TableCell>
+                        <TableCell>{event.name.text}</TableCell>
+                        <TableCell>{event.status}</TableCell>
+                        <TableCell>{event.capacity}</TableCell>
+                    </TableRow>
+                ))}
+            </TableBody>
+        </Table>
+    </TableContainer>
   )
 }
