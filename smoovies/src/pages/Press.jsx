966693cabf5f8d7cdaa42363900973fac594aa79
Nicholas Ameyaw Abuaku <abuaku1400@gmail.com>
2024-02-07 13:46:17 +0000

    Press page with card component

--- smoovies/src/pages/Press.jsx
+++ smoovies/src/pages/Press.jsx
@@ -1,20 +1,6 @@
 import React from "react";
 import { Skeleton, Stack } from "@mui/material";
+import { PressCard } from "../components/PressCard";
 export const Press = () => {
-  return (
-    <Stack direction={"row"}>
-      <Skeleton
-        variant="rectangular"
-        width={974}
-        height={465}
-        animation="wave"
-      />
-      <Skeleton
-        variant="rectangular"
-        width={929}
-        height={465}
-        animation="wave"
-      />
-    </Stack>
-  );
+  return <PressCard />;
 };
