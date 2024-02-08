10f6bb46081292f8440b553f93949009db1b73be
Nicholas Ameyaw Abuaku <abuaku1400@gmail.com>
2024-02-08 16:02:46 +0000

    All pages lazy load

--- smoovies/src/App.jsx
+++ smoovies/src/App.jsx
@@ -1,31 +1,35 @@
 import "bootstrap/dist/css/bootstrap.css";
 import "./App.css";
+import React, { lazy, Suspense } from "react";
 import { Footer } from "./components/Footer";
 import { Home } from "./pages/Home";
-import { Routes, Route } from "react-router-dom";
-import { Contact } from "./pages/Contact.jsx";
-import { FilmClub } from "./pages/FlimClub.jsx";
+import { Routes, Route, Router } from "react-router-dom";
 import { Navigation2 } from "./components/Navigation2.jsx";
-import { About } from "./pages/About.jsx";
-import { Press } from "./pages/Press.jsx";
-import { EditLatestScreening } from "./components/back-end/EditLatestScreening.jsx";
-import { Dashboard } from "./pages/back-end/Dashboard.jsx";
-import { LatestScreeningPage } from "./pages/back-end/LatestScreeningPage.jsx";
+const Contact = lazy(() => import("./pages/Contact"));
+const FilmClub = lazy(() => import("./pages/FlimClub"));
+const Press = lazy(() => import("./pages/Press"));
+const About = lazy(() => import("./pages/About"));
+const Dashboard = lazy(() => import("./Pages/back-end/Dashboard"));
+const LatestScreeningPage = lazy(() =>
+  import("./Pages/back-end/LatestScreeningPage")
+);
 function App() {
   return (
     <>
       <Navigation2 />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/film-clubs" element={<FilmClub />} />
-        <Route path="/press" element={<Press />} />
-        <Route path="/about" element={<Dashboard />} />
-        <Route
-          path="/dashboard/edit-latest-screening"
-          element={<LatestScreeningPage />}
-        />
-      </Routes>
+      <Suspense fallback={<h1>Loading...</h1>}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/film-clubs" element={<FilmClub />} />
+          <Route path="/press" element={<Press />} />
+          <Route path="/about" element={<Dashboard />} />
+          <Route
+            path="/dashboard/edit-latest-screening"
+            element={<LatestScreeningPage />}
+          />
+        </Routes>
+      </Suspense>
       <Footer />
     </>
   );
