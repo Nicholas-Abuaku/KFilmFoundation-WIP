18d89e26671e50c075ec0c7c51181bc941c1f6e1
Nicholas Ameyaw Abuaku <abuaku1400@gmail.com>
2024-02-08 16:06:13 +0000

    Changed export to default and created grid for page layout

--- smoovies/src/pages/Press.jsx
+++ smoovies/src/pages/Press.jsx
@@ -1,6 +1,24 @@
 import React from "react";
-import { Skeleton, Stack } from "@mui/material";
+import { Skeleton, Stack, Grid, Typography } from "@mui/material";
 import { PressCard } from "../components/PressCard";
-export const Press = () => {
-  return <PressCard />;
+import { PressCardGrid } from "../components/back-end/PressCardGrid";
+const Press = () => {
+  return (
+    <Grid container justifyContent={"center"} alignItems={"center"}>
+      <Grid item xs={12}>
+        <Typography
+          variant="h3"
+          textAlign={"center"}
+          sx={{ textDecoration: "underline" }}
+        >
+          Press
+        </Typography>
+      </Grid>
+      <Grid>
+        <PressCardGrid />
+      </Grid>
+    </Grid>
+  );
 };
+
+export default Press;
