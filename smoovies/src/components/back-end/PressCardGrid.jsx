b982d3fb484e6c781ef844c55dc1d1f802ea0ff6
Nicholas Ameyaw Abuaku <abuaku1400@gmail.com>
2024-02-08 16:03:36 +0000

    created press card grid

---
+++ smoovies/src/components/back-end/PressCardGrid.jsx
@@ -0,0 +1,39 @@
+import { Grid } from "@mui/material";
+import { PressCard } from "../PressCard";
+import React from "react";
+export const PressCardGrid = () => {
+  return (
+    <Grid container>
+      <Grid item>
+        <PressCard title="Jiggle Batty" />
+      </Grid>
+      <Grid item>
+        <PressCard title="Jiggle Batty" />
+      </Grid>
+      <Grid item>
+        <PressCard title="Jiggle Batty" />
+      </Grid>
+      <Grid item>
+        <PressCard title="Jiggle Batty" />
+      </Grid>
+      <Grid item>
+        <PressCard title="Jiggle Batty" />
+      </Grid>
+      <Grid item>
+        <PressCard title="Jiggle Batty" />
+      </Grid>
+      <Grid item>
+        <PressCard title="Jiggle Batty" />
+      </Grid>
+      <Grid item>
+        <PressCard title="Jiggle Batty" />
+      </Grid>
+      <Grid item>
+        <PressCard title="Jiggle Batty" />
+      </Grid>
+      <Grid item>
+        <PressCard title="Jiggle Batty" />
+      </Grid>
+    </Grid>
+  );
+};
