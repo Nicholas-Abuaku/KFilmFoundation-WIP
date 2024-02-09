f4059835395ff955347c6cad0246f9f7c2ce061f
Nicholas Ameyaw Abuaku <abuaku1400@gmail.com>
2024-02-09 14:48:37 +0000

    Social media links now part of footer

--- smoovies/src/components/Footer.jsx
+++ smoovies/src/components/Footer.jsx
@@ -1,28 +1,44 @@
-import React from 'react'
-import {Typography, useTheme, useMediaQuery, ThemeProvider, createTheme } from '@mui/material';
-import FacebookIcon from '@mui/icons-material/Facebook';
-import InstagramIcon from '@mui/icons-material/Instagram';
+import React from "react";
+import {
+  Typography,
+  useTheme,
+  useMediaQuery,
+  ThemeProvider,
+  createTheme,
+  Grid,
+} from "@mui/material";
+import { SocialMediaLinks } from "./SocialMediaLinks";
 export const Footer = () => {
   const theme = useTheme();
   const isMobile = useMediaQuery(theme.breakpoints.down("md"));
   const textTheme = createTheme();
-  textTheme.typography.h6={
-    fontSize: '0.1 rem',
-    fontFamily:'Open Sans, arial, sans-serif',
-    '@media (min-width:600px)': {
-      fontSize: '1rem',
+  textTheme.typography.h6 = {
+    fontSize: "0.1 rem",
+    fontFamily: "Open Sans, arial, sans-serif",
+    "@media (min-width:600px)": {
+      fontSize: "1rem",
     },
-    [theme.breakpoints.up('md')]: {
-      fontSize: '1rem',
+    [theme.breakpoints.up("md")]: {
+      fontSize: "1rem",
     },
-  }
+  };
   return (
-    <>
-    <ThemeProvider theme={textTheme}>
-    <Typography variant='h6' textAlign={'center'}>COPYRIGHT © 2024 KENT FILM FOUNDATION - ALL RIGHTS RESERVED.</Typography>
-    </ThemeProvider>
-    </>
-  )
-}
-
-
+    <Grid
+      container
+      justifyContent={"center"}
+      alignItems={"center"}
+      marginTop={4}
+    >
+      <Grid item xs={12}>
+        <SocialMediaLinks />
+      </Grid>
+      <Grid item xs={12}>
+        <ThemeProvider theme={textTheme}>
+          <Typography variant="h6" textAlign={"center"}>
+            COPYRIGHT © 2024 KENT FILM FOUNDATION - ALL RIGHTS RESERVED.
+          </Typography>
+        </ThemeProvider>
+      </Grid>
+    </Grid>
+  );
+};
