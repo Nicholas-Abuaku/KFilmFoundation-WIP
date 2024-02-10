255f3f2ef4b4af67eb7db0615ca04681c676bba3
Nicholas Ameyaw Abuaku <abuaku1400@gmail.com>
2024-02-10 12:33:22 +0000

    Changing layout of about

--- smoovies/src/pages/About.jsx
+++ smoovies/src/pages/About.jsx
@@ -34,56 +34,62 @@ const About = () => {
   };
   return (
     <Grid container spacing={1} justifyContent={"center"} alignItems={"center"}>
-      <Grid item xs={12} justifyContent={"center"} alignItems={"center"}>
-        <img src="https://img1.wsimg.com/isteam/ip/c75f83f5-5376-471b-af2d-7c3435beb175/ff5ef498a6974abbf9f8cbb269fac47a628a60a5.jpg" />
+      <Grid item xs={12}>
+        <img
+          src="https://img1.wsimg.com/isteam/ip/c75f83f5-5376-471b-af2d-7c3435beb175/ff5ef498a6974abbf9f8cbb269fac47a628a60a5.jpg"
+          style={{ minWidth: "400px", minHeight: "360px" }}
+        />
       </Grid>
-      <Grid
-        item
-        container
-        xs={12}
-        marginLeft={0}
-        direction={"row"}
-        spacing={4}
-        marginTop={3}
-      >
-        <Grid item>
-          <ThemeProvider theme={textTheme}>
-            <Typography color={"green"} variant="h6">
-              HISTORY - HOW IT ALL BEGAN
+      <Grid item xs={12}>
+        <Typography variant="h3" sx={{ textDecoration: "underline" }}>
+          About
+        </Typography>
+      </Grid>
+      <Grid item xs={12} container>
+        <Grid item xs={6}>
+          <Stack spacing={2}>
+            <Typography sx={{ fontSize: "4px" }}>
+              "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat
+              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+              sunt in culpa qui officia deserunt mollit anim id est laborum."
             </Typography>
-          </ThemeProvider>
+            <Typography sx={{ fontSize: "4px" }}>
+              "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat
+              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+              sunt in culpa qui officia deserunt mollit anim id est laborum."
+            </Typography>
+            <Typography sx={{ fontSize: "4px" }}>
+              "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat
+              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+              sunt in culpa qui officia deserunt mollit anim id est laborum."
+            </Typography>
+          </Stack>
         </Grid>
-        <Grid item>
-          <Typography fontFamily={"Open Sans, arial, sans-serif"}>
-            Kent Film Foundation began as a non-for-profit arm of a film
-            production company based in Thanet , Kent an area of high
-            deprivation where young people suffer from "Poverty of Opportunity".
-          </Typography>
-        </Grid>
-        <Grid item>
-          <Typography fontFamily={"Open Sans, arial, sans-serif"}>
-            A local group of filmmakers attached training elements for young
-            people to a series of feature film shoots. This successfully led
-            talented young people into entry level film industry jobs, film
-            schools, universities and drama schools for those interested in
-            careers in media.{" "}
-          </Typography>
-        </Grid>
-        <Grid item>
-          <Typography fontFamily={"Open Sans, arial, sans-serif"}>
-            This inspired one of the filmmakers who had also grown up in poverty
-            herself, to create the Kent Film Foundation, an organisation with
-            charitable status in the hope of hiring professional filmmakers to
-            mentor young people in film workshops that would be FREE to every
-            participant.
-          </Typography>
-        </Grid>
-        <Grid item paddingBottom={10}>
-          <Typography fontFamily={"Open Sans, arial, sans-serif"}>
-            Since its inception in 2010 the charity has an excellent track
-            record in achieving its outcomes. It recently received a prestigious
-            nomination in the Colyer Fergusson 50th Anniversary Awards in the
-            category "Unlocking Opportunities"
+        <Grid item xs={6} marginTop={60}>
+          <img
+            src="https://img1.wsimg.com/isteam/ip/c75f83f5-5376-471b-af2d-7c3435beb175/The_ParalympicsGB_Sports_Fest_has_been_a_great.jpg"
+            style={{
+              borderRadius: "50%",
+              maxHeight: "300px",
+            }}
+          />
+          <Typography>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorum
+            possimus saepe sunt alias provident quidem fuga quod unde vel
+            corrupti! Libero assumenda voluptates porro natus eaque voluptate
+            tempore tempora quas!
           </Typography>
         </Grid>
       </Grid>
