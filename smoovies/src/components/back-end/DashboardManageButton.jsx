b49a92eee9a76acae768a435f10495add1f450c2
Nicholas Ameyaw Abuaku <abuaku1400@gmail.com>
2024-02-10 12:31:36 +0000

    prettier reformatted

--- smoovies/src/components/back-end/DashboardManageButton.jsx
+++ smoovies/src/components/back-end/DashboardManageButton.jsx
@@ -1,19 +1,35 @@
-import { Card, CardMedia, CardContent, Stack, Typography, Icon, CardActionArea } from '@mui/material'
-import React from 'react'
-import ManageHistoryIcon from '@mui/icons-material/ManageHistory';
+import {
+  Card,
+  CardMedia,
+  CardContent,
+  Stack,
+  Typography,
+  Icon,
+  CardActionArea,
+} from "@mui/material";
+import React from "react";
+import ManageHistoryIcon from "@mui/icons-material/ManageHistory";
 export const DashboardManageButton = (props) => {
   return (
-    <CardActionArea sx={{ width:'300px', height:'140px'}}>
-    <Card sx={{ width:'300px', height:'140px', background:'linear-gradient(90deg, rgba(0,36,4,1) 0%, rgba(43,125,74,1) 50%)', color:'white' }}>
-        <CardContent> 
-            <Stack direction={'row'} spacing={1}>
-            <Typography variant='h4'>Manage Film Clubs</Typography>
-            <Icon sx={{ height:'40px' }}>
-                <ManageHistoryIcon/>
+    <CardActionArea sx={{ width: "300px", height: "140px" }}>
+      <Card
+        sx={{
+          width: "300px",
+          height: "140px",
+          background:
+            "linear-gradient(90deg, rgba(0,36,4,1) 0%, rgba(43,125,74,1) 50%)",
+          color: "white",
+        }}
+      >
+        <CardContent>
+          <Stack direction={"row"} spacing={1}>
+            <Typography variant="h4">Manage Film Clubs</Typography>
+            <Icon sx={{ height: "40px" }}>
+              <ManageHistoryIcon />
             </Icon>
-            </Stack>
+          </Stack>
         </CardContent>
-    </Card>
+      </Card>
     </CardActionArea>
-  )
-}
+  );
+};
