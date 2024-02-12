432424e034ed767bead628934425a6f7bc8b4e39
Nicholas Ameyaw Abuaku <abuaku1400@gmail.com>
2024-02-12 12:37:07 +0000

    removed unused route import and added paths for film club manager page and edit film club (for testing purposes)

--- smoovies/src/App.jsx
+++ smoovies/src/App.jsx
@@ -3,7 +3,7 @@ import "./App.css";
 import React, { lazy, Suspense } from "react";
 import { Footer } from "./components/Footer";
 import { Home } from "./pages/Home";
-import { Routes, Route, Router } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import { Navigation2 } from "./components/Navigation2.jsx";
 const Contact = lazy(() => import("./pages/Contact"));
 const FilmClub = lazy(() => import("./pages/FlimClub"));
@@ -13,6 +13,8 @@ const Dashboard = lazy(() => import("./Pages/back-end/Dashboard"));
 const LatestScreeningPage = lazy(() =>
   import("./Pages/back-end/LatestScreeningPage")
 );
+import { FilmClubManager } from "./pages/back-end/FilmClubManager.jsx";
+import { EditFilmClub } from "./pages/back-end/EditFilmClub.jsx";
 function App() {
   return (
     <>
@@ -27,6 +29,12 @@ function App() {
           <Route
             path="/dashboard/edit-latest-screening"
             element={<LatestScreeningPage />}
+          />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/dashboard/film-clubs" element={<FilmClubManager />} />
+          <Route
+            path="/dashboard/film-clubs/edit/:id"
+            element={<EditFilmClub />}
           />
         </Routes>
       </Suspense>
