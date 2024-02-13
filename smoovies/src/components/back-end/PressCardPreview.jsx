aaa9d91b81213b1ad8885f9b030a623ac78e8461
Nicholas Ameyaw Abuaku <abuaku1400@gmail.com>
2024-02-13 13:45:25 +0000

    Card to preview when making changes to or creating a new press article (Might replace with the original press card)

---
+++ smoovies/src/components/back-end/PressCardPreview.jsx
@@ -0,0 +1,49 @@
+import {
+  Card,
+  Paper,
+  CardMedia,
+  CardContent,
+  Stack,
+  Typography,
+  Button,
+} from "@mui/material";
+import React from "react";
+import { Link } from "react-router-dom";
+
+export const PressCardPreview = (props) => {
+  return (
+    <Card
+      width={"100%"}
+      sx={{
+        backgroundColor: "#339465",
+        minHeight: "462px",
+        width: "369.5px",
+        color: "white",
+        marginLeft: "10px",
+        marginBottom: "30px",
+        borderRadius: "0px",
+      }}
+    >
+      <CardMedia component={"img"} height={"300px"} src={props.img} />
+      <CardContent>
+        <Stack spacing={0}>
+          <Typography
+            textAlign={"center"}
+            fontWeight={"bold"}
+            variant="subtitle1"
+          >
+            {props.source}
+          </Typography>
+          <Typography textAlign={"center"} variant="body1">
+            {props.title}
+          </Typography>
+        </Stack>
+        <Stack alignItems={"center"} justifyContent={"center"} marginTop={2}>
+          <Button variant="outlined" color="inherit">
+            Learn More
+          </Button>
+        </Stack>
+      </CardContent>
+    </Card>
+  );
+};
