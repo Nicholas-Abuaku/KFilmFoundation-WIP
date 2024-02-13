f09a3bdb05263c8d1ab85e46f6b6864a3c8fea5f
Nicholas Ameyaw Abuaku <abuaku1400@gmail.com>
2024-02-13 12:54:46 +0000

    Film Club manage page now has back button

--- smoovies/src/pages/back-end/FilmClubManager.jsx
+++ smoovies/src/pages/back-end/FilmClubManager.jsx
@@ -1,5 +1,26 @@
-import React from "react";
+import React, { useContext, useState } from "react";
 import { FilmClubTable } from "../../components/back-end/FilmClubTable";
+import { ManageTableContext } from "../../Contexts/ManageTableContext";
+import { Button, Grid, Stack, IconButton } from "@mui/material";
+import ArrowBackIcon from "@mui/icons-material/ArrowBack";
+import { Link } from "react-router-dom";
 export const FilmClubManager = () => {
-  return <FilmClubTable />;
+  const [tableUpdate, setTableUpdate] = useState(false);
+  return (
+    <Grid container>
+      <Grid item xs={12}>
+        <Stack direction={"row"} spacing={2}>
+          <IconButton component={Link} to={"/dashboard"}>
+            <ArrowBackIcon />
+          </IconButton>
+          <h1>Film Clubs</h1>
+        </Stack>
+      </Grid>
+      <Grid item xs={12}>
+        <ManageTableContext.Provider value={{ tableUpdate, setTableUpdate }}>
+          <FilmClubTable />
+        </ManageTableContext.Provider>
+      </Grid>
+    </Grid>
+  );
 };
