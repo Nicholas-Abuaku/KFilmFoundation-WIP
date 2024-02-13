85d6dd42d4b43d470d823afaf3c487280c72debe
Nicholas Ameyaw Abuaku <abuaku1400@gmail.com>
2024-02-13 12:54:04 +0000

    Actions button for press form. One will be deleted

---
+++ smoovies/src/components/back-end/PressActions.jsx
@@ -0,0 +1,82 @@
+import React, { useState, useReducer, useContext } from "react";
+import { ManageTableContext } from "../../Contexts/ManageTableContext";
+import Button from "@mui/material/Button";
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
+import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
+import { Link } from "react-router-dom";
+import Snackbar from "@mui/material/Snackbar";
+import axios from "axios";
+export const PressActions = (props) => {
+  const { tableUpdate, setTableUpdate } = useContext(ManageTableContext);
+  const [anchorEl, setAnchorEl] = useState(null);
+  const open = Boolean(anchorEl);
+  const [openSnack, setOpenSnack] = useState();
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+    console.log(props.id);
+  };
+
+  const API_KEY = import.meta.env.VITE_Laravel_API_KEY;
+  const headers = {
+    Authorization: `Bearer ${API_KEY}`,
+    "Content-Type": "multipart/form-data",
+  };
+
+  const handleDelete = async () => {
+    try {
+      const response = await axios.delete(
+        "http://localhost:8000/api/film-clubs/" + props.id,
+        { headers }
+      );
+      setOpenSnack(true);
+      setTableUpdate(true);
+      console.log(response);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  return (
+    <>
+      <Snackbar
+        open={openSnack}
+        autoHideDuration={200000}
+        onClose={() => console.log("Close")}
+        message="Film Club Deleted"
+      />
+      <Button
+        variant="contained"
+        color="warning"
+        id="film-clubs-actions-button"
+        onClick={handleClick}
+        aria-controls={open ? "film-clubs-actions" : undefined}
+        aria-haspopup="true"
+        aria-expanded={open ? "true" : undefined}
+        endIcon={<ArrowDropDownIcon />}
+      >
+        Actions
+      </Button>
+      <Menu
+        id="film-clubs-actions"
+        anchorEl={anchorEl}
+        open={open}
+        MenuListProps={{ "aria-labelledby": "film-clubs-actions-button" }}
+        onClose={handleClose}
+      >
+        <MenuItem
+          onClick={handleClose}
+          component={Link}
+          to={"/dashboard/film-clubs/edit/" + props.id}
+        >
+          Edit
+        </MenuItem>
+        <MenuItem onClick={handleDelete}>Delete</MenuItem>
+      </Menu>
+    </>
+  );
+};
---
+++ smoovies/src/components/back-end/PressActionsButton.jsx
@@ -0,0 +1,78 @@
+import { Button, Menu, MenuItem, Snackbar } from "@mui/material";
+import React, { useState, useContext } from "react";
+import { ManageTableContext } from "../../Contexts/ManageTableContext";
+import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
+import axios from "axios";
+import { Link } from "react-router-dom";
+export const PressActionsButton = (props) => {
+  const [anchorEl, setAnchorEl] = useState(null);
+  const [openSnack, setOpenSnack] = useState(false);
+  const { tableUpdate, setTableUpdate } = useContext(ManageTableContext);
+  const open = Boolean(anchorEl);
+  const API_KEY = import.meta.env.VITE_Laravel_API_KEY;
+  const headers = {
+    Authorization: `Bearer ${API_KEY}`,
+    "Content-Type": "multipart/form-data",
+  };
+
+  function handleClose() {
+    setAnchorEl(null);
+    console.log(props.id);
+  }
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleDelete = async () => {
+    try {
+      const response = await axios.delete(
+        "http://localhost:8000/api/press/" + props.id,
+        { headers }
+      );
+      setOpenSnack(true);
+      setTableUpdate(true);
+      console.log(response);
+      props.onDelete();
+    } catch (err) {
+      console.log(err);
+    }
+  };
+  return (
+    <>
+      <Snackbar
+        open={openSnack}
+        autoHideDuration={200000}
+        onClose={() => console.log("Close")}
+        message="Film Club Deleted"
+      />
+      <Button
+        variant="contained"
+        color="warning"
+        id="press-actions-button"
+        onClick={handleClick}
+        aria-controls={open ? "press-actions" : undefined}
+        aria-haspopup="true"
+        aria-expanded={open ? "true" : undefined}
+        endIcon={<ArrowDropDownIcon />}
+      >
+        Actions
+      </Button>
+      <Menu
+        id="presss-actions"
+        anchorEl={anchorEl}
+        open={open}
+        MenuListProps={{ "aria-labelledby": "press-actions-button" }}
+        onClose={handleClose}
+      >
+        <MenuItem
+          onClick={handleClose}
+          component={Link}
+          to={"/dashboard/press/edit/" + props.id}
+        >
+          Edit
+        </MenuItem>
+        <MenuItem onClick={handleDelete}>Delete</MenuItem>
+      </Menu>
+    </>
+  );
+};
