00397a806a5a5a5f73f9eb93bb61f329c536d55c
Nicholas Ameyaw Abuaku <abuaku1400@gmail.com>
2024-02-13 12:52:50 +0000

    Now links to dashboard press manage page

--- smoovies/src/components/back-end/DashboardManageNewsButton.jsx
+++ smoovies/src/components/back-end/DashboardManageNewsButton.jsx
@@ -1,19 +1,40 @@
-import { Card, CardMedia, CardContent, Stack, Typography, Icon, CardActionArea } from '@mui/material'
-import React from 'react'
-import ManageHistoryIcon from '@mui/icons-material/ManageHistory';
+import {
+  Card,
+  CardMedia,
+  CardContent,
+  Stack,
+  Typography,
+  Icon,
+  CardActionArea,
+} from "@mui/material";
+import React from "react";
+import ManageHistoryIcon from "@mui/icons-material/ManageHistory";
+import { Link } from "react-router-dom";
 export const DashboardManageNewsButton = (props) => {
   return (
-    <CardActionArea sx={{ width:'300px', height:'140px'}}>
-    <Card sx={{ width:'300px', height:'140px', background:'linear-gradient(90deg, rgba(0,36,4,1) 0%, rgba(43,125,74,1) 50%)', color:'white' }}>
-        <CardContent> 
-            <Stack direction={'row'} spacing={1}>
-            <Typography variant='h4'>Manage Press Articles</Typography>
-            <Icon sx={{ height:'40px' }}>
-                <ManageHistoryIcon/>
+    <CardActionArea
+      sx={{ width: "300px", height: "140px" }}
+      component={Link}
+      to={"/dashboard/press"}
+    >
+      <Card
+        sx={{
+          width: "300px",
+          height: "140px",
+          background:
+            "linear-gradient(90deg, rgba(0,36,4,1) 0%, rgba(43,125,74,1) 50%)",
+          color: "white",
+        }}
+      >
+        <CardContent>
+          <Stack direction={"row"} spacing={1}>
+            <Typography variant="h4">Manage Press Articles</Typography>
+            <Icon sx={{ height: "40px" }}>
+              <ManageHistoryIcon />
             </Icon>
-            </Stack>
+          </Stack>
         </CardContent>
-    </Card>
+      </Card>
     </CardActionArea>
-  )
-}
+  );
+};
