3b5bdad4089af3dee938cf4fe9f565558e24ee17
Nicholas Ameyaw Abuaku <abuaku1400@gmail.com>
2024-02-14 13:28:39 +0000

    Now shows a loading skeleton for press articles while being fetched from API

--- smoovies/src/components/back-end/PressCardGrid.jsx
+++ smoovies/src/components/back-end/PressCardGrid.jsx
@@ -1,39 +1,48 @@
-import { Grid } from "@mui/material";
+import { Grid, Skeleton } from "@mui/material";
 import { PressCard } from "../PressCard";
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 export const PressCardGrid = () => {
+  const [pressData, setPressData] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const fetchPressData = async () => {
+    try {
+      axios.get("http://localhost:8000/api/press").then((res) => {
+        console.log(res.data);
+        setPressData(res.data);
+        setIsLoading(true);
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    fetchPressData();
+  }, []);
   return (
-    <Grid container>
-      <Grid item>
-        <PressCard title="Jiggle Batty" />
-      </Grid>
-      <Grid item>
-        <PressCard title="Jiggle Batty" />
-      </Grid>
-      <Grid item>
-        <PressCard title="Jiggle Batty" />
-      </Grid>
-      <Grid item>
-        <PressCard title="Jiggle Batty" />
-      </Grid>
-      <Grid item>
-        <PressCard title="Jiggle Batty" />
-      </Grid>
-      <Grid item>
-        <PressCard title="Jiggle Batty" />
-      </Grid>
-      <Grid item>
-        <PressCard title="Jiggle Batty" />
-      </Grid>
-      <Grid item>
-        <PressCard title="Jiggle Batty" />
-      </Grid>
-      <Grid item>
-        <PressCard title="Jiggle Batty" />
-      </Grid>
-      <Grid item>
-        <PressCard title="Jiggle Batty" />
-      </Grid>
+    <Grid container spacing={2}>
+      {isLoading
+        ? pressData.map((article) => (
+            <Grid item xs={3}>
+              <PressCard
+                newsSource={article.news_source}
+                articleTitle={article.article_title}
+                articleUrl={article.article_url}
+                image={"http://localhost:8000/storage/" + article.image}
+              />
+            </Grid>
+          ))
+        : Array.from({ length: 4 }).map((_, index) => (
+            <Grid item key={index} xs={3}>
+              <Skeleton
+                variant="rect"
+                width={369.5}
+                height={462}
+                animation="wave"
+              />
+            </Grid>
+          ))}
     </Grid>
   );
 };
