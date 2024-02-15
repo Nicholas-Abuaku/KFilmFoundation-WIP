08d1cd0b2d9cf1a44a6dd6b8c245cf6c34366e33
Nicholas Ameyaw Abuaku <abuaku1400@gmail.com>
2024-02-15 17:04:25 +0000

    React Helmet + title and meta tags

--- kent-film-foundation/src/pages/Press.jsx
+++ kent-film-foundation/src/pages/Press.jsx
@@ -1,21 +1,33 @@
 import { Grid, Typography } from "@mui/material";
 import { PressCardGrid } from "../components/back-end/PressCardGrid";
+import { Helmet } from "react-helmet-async";
 const Press = () => {
   return (
-    <Grid container justifyContent={"center"} alignItems={"center"}>
-      <Grid item xs={12}>
-        <Typography
-          variant="h3"
-          textAlign={"center"}
-          sx={{ textDecoration: "underline" }}
-        >
-          Press
-        </Typography>
+    <>
+      <Helmet>
+        <title>Press</title>
+        <meta
+          name="description"
+          content="Discover Kent Film Foundation's news mentions, articles, and impactful contributions to the film industry. Stay informed about our latest initiatives and partnerships."
+        />
+        <link rel="canonical" href="http://localhost:5173/press" />
+      </Helmet>
+      <Grid container justifyContent={"center"} alignItems={"center"}>
+        <Grid item xs={12}>
+          <Typography
+            variant="h3"
+            textAlign={"center"}
+            component={"h2"}
+            sx={{ textDecoration: "underline" }}
+          >
+            Press
+          </Typography>
+        </Grid>
+        <Grid>
+          <PressCardGrid />
+        </Grid>
       </Grid>
-      <Grid>
-        <PressCardGrid />
-      </Grid>
-    </Grid>
+    </>
   );
 };
 
