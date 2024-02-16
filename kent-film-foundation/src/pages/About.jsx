e342b4186c4f0b57c10a96d9036bb887b5542772
Nicholas Ameyaw Abuaku <abuaku1400@gmail.com>
2024-02-16 10:46:35 +0000

    added meta data and separate mobile layout

--- kent-film-foundation/src/pages/About.jsx
+++ kent-film-foundation/src/pages/About.jsx
@@ -12,72 +12,146 @@ import React from "react";
 import { Helmet } from "react-helmet-async";
 const About = () => {
   const theme = useTheme();
-  const textTheme = createTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.down("md"));
+
   return (
     <>
+      <Helmet>
+        <title>About</title>
+        <meta
+          name="description"
+          content="Explore Kent Film Foundation's history, mission, and impactful film projects. Join us in the world of cinema and storytelling."
+        />
+        <link rel="canonical" href="http://localhost:5173/about" />
+      </Helmet>
       <Grid
         container
         spacing={1}
         justifyContent={"center"}
         alignItems={"center"}
       >
-        <Grid item xs={12}>
+        <Grid item xs={12} md={12}>
           <img
             src="https://img1.wsimg.com/isteam/ip/c75f83f5-5376-471b-af2d-7c3435beb175/ff5ef498a6974abbf9f8cbb269fac47a628a60a5.jpg"
-            style={{ minWidth: "400px", minHeight: "360px" }}
+            width={"100%"}
+            height={300}
+            loading="eager"
+            title="banner"
+            alt="banner"
           />
         </Grid>
-        <Grid item xs={12}>
-          <Typography variant="h3" sx={{ textDecoration: "underline" }}>
-            About
-          </Typography>
-        </Grid>
-        <Grid item xs={12} container>
-          <Grid item xs={6}>
-            <Stack spacing={2}>
-              <Typography>
-                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-                enim ad minim veniam, quis nostrud exercitation ullamco laboris
-                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
-                in reprehenderit in voluptate velit esse cillum dolore eu fugiat
-                nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-                sunt in culpa qui officia deserunt mollit anim id est laborum."
-              </Typography>
-              <Typography>
-                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-                enim ad minim veniam, quis nostrud exercitation ullamco laboris
-                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
-                in reprehenderit in voluptate velit esse cillum dolore eu fugiat
-                nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-                sunt in culpa qui officia deserunt mollit anim id est laborum."
-              </Typography>
-              <Typography>
-                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-                enim ad minim veniam, quis nostrud exercitation ullamco laboris
-                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
-                in reprehenderit in voluptate velit esse cillum dolore eu fugiat
-                nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-                sunt in culpa qui officia deserunt mollit anim id est laborum."
-              </Typography>
-            </Stack>
-          </Grid>
-          <Grid item xs={6} marginTop={60}>
-            <img
-              src="https://img1.wsimg.com/isteam/ip/c75f83f5-5376-471b-af2d-7c3435beb175/The_ParalympicsGB_Sports_Fest_has_been_a_great.jpg"
-              style={{
-                borderRadius: "50%",
-                maxHeight: "300px",
-              }}
-            />
-            <Typography>
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorum
-              possimus saepe sunt alias provident quidem fuga quod unde vel
-              corrupti! Libero assumenda voluptates porro natus eaque voluptate
-              tempore tempora quas!
+        <Grid item container>
+          <Grid item xs={7} md={7} marginRight={0}>
+            <Typography variant="h3" component={"h2"}>
+              About
             </Typography>
+            <hr style={{ width: "300px" }} />
+            {isMobile ? (
+              <Stack direction={"column"} spacing={2} width={200}>
+                <Typography variant="body1" component={"h3"}>
+                  Kent Film Foundation began as a non-for-profit arm of a film
+                  production company based in Thanet , Kent an area of high
+                  deprivation where young people suffer from "Poverty of
+                  Opportunity".
+                </Typography>
+                <Typography variant="body1" component={"h4"}>
+                  A local group of filmmakers attached training elements for
+                  young people to a series of feature film shoots. This
+                  successfully led talented young people into entry level film
+                  industry jobs, film schools, universities and drama schools
+                  for those interested in careers in media.
+                </Typography>
+                <Typography variant="body1">
+                  This inspired one of the filmmakers who had also grown up in
+                  poverty herself, to create the Kent Film Foundation, an
+                  organisation with charitable status in the hope of hiring
+                  professional filmmakers to mentor young people in film
+                  workshops that would be FREE to every participant.
+                </Typography>
+                <Typography variant="body1">
+                  Since its inception in 2010 the charity has an excellent track
+                  record in achieving its outcomes. It recently received a
+                  prestigious nomination in the Colyer Fergusson 50th
+                  Anniversary Awards in the category "Unlocking Opportunities"
+                </Typography>
+              </Stack>
+            ) : (
+              <Stack direction={"column"} spacing={2} width={900}>
+                <Typography variant="h5" component={"h3"}>
+                  Kent Film Foundation began as a non-for-profit arm of a film
+                  production company based in Thanet , Kent an area of high
+                  deprivation where young people suffer from "Poverty of
+                  Opportunity".
+                </Typography>
+                <Typography variant="h5" component={"h4"}>
+                  A local group of filmmakers attached training elements for
+                  young people to a series of feature film shoots. This
+                  successfully led talented young people into entry level film
+                  industry jobs, film schools, universities and drama schools
+                  for those interested in careers in media.
+                </Typography>
+                <Typography variant="h5">
+                  This inspired one of the filmmakers who had also grown up in
+                  poverty herself, to create the Kent Film Foundation, an
+                  organisation with charitable status in the hope of hiring
+                  professional filmmakers to mentor young people in film
+                  workshops that would be FREE to every participant.
+                </Typography>
+                <Typography variant="h5">
+                  Since its inception in 2010 the charity has an excellent track
+                  record in achieving its outcomes. It recently received a
+                  prestigious nomination in the Colyer Fergusson 50th
+                  Anniversary Awards in the category "Unlocking Opportunities"
+                </Typography>
+              </Stack>
+            )}
+          </Grid>
+          <Grid item xs={5} md={5} marginTop={20} marginLeft={0}>
+            {isMobile ? (
+              <Stack
+                alignItems={"center"}
+                justifyContent={"center"}
+                width={150}
+              >
+                <img
+                  style={{
+                    borderRadius: "50%",
+                    width: "100px",
+                    marginBottom: "10px",
+                  }}
+                  alt="'Aiming High' is a film made by our young filmmakers about Ian Payne a wheelchair tennis player."
+                  title="Ian Payne"
+                  loading="eager"
+                  src="https://img1.wsimg.com/isteam/ip/c75f83f5-5376-471b-af2d-7c3435beb175/The_ParalympicsGB_Sports_Fest_has_been_a_great.jpg/:/cr=t:0%25,l:14.5%25,w:71%25,h:100%25/rs=w:400,h:400,cg:true"
+                />
+                <Typography variant="caption">
+                  "Aiming High" is a film made by our young filmmakers about Ian
+                  Payne a wheelchair tennis player.
+                </Typography>
+              </Stack>
+            ) : (
+              <Stack
+                alignItems={"center"}
+                justifyContent={"center"}
+                width={400}
+              >
+                <img
+                  style={{
+                    borderRadius: "50%",
+                    width: "300px",
+                    marginBottom: "10px",
+                  }}
+                  alt="'Aiming High' is a film made by our young filmmakers about Ian Payne a wheelchair tennis player."
+                  title="Ian Payne"
+                  loading="eager"
+                  src="https://img1.wsimg.com/isteam/ip/c75f83f5-5376-471b-af2d-7c3435beb175/The_ParalympicsGB_Sports_Fest_has_been_a_great.jpg/:/cr=t:0%25,l:14.5%25,w:71%25,h:100%25/rs=w:400,h:400,cg:true"
+                />
+                <Typography variant="caption" fontSize={15}>
+                  "Aiming High" is a film made by our young filmmakers about Ian
+                  Payne a wheelchair tennis player.
+                </Typography>
+              </Stack>
+            )}
           </Grid>
         </Grid>
       </Grid>
