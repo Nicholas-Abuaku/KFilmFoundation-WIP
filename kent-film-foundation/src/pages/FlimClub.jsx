673822e31f2a70c9576d171c6339075fad7bfc23
Nicholas Ameyaw Abuaku <abuaku1400@gmail.com>
2024-02-16 14:38:06 +0000

    Imported film club display grid

--- kent-film-foundation/src/pages/FlimClub.jsx
+++ kent-film-foundation/src/pages/FlimClub.jsx
@@ -16,6 +16,7 @@ import {
   createTheme,
 } from "@mui/material";
 import { FilmClubDisplayCard } from "../components/FilmClubDisplayCard";
+import { FilmClubDisplayGrid } from "../components/FilmClubDisplayGrid";
 
 const FilmClub = () => {
   const theme = useTheme();
@@ -56,7 +57,7 @@ const FilmClub = () => {
               />
               <CardContent>
                 <Stack spacing={2}>
-                  <Typography textAlign={"center"} variant="h5" color={"white"}>
+                  <Typography textAlign={"center"} variant="h6" color={"white"}>
                     FREE YOUTH FILM CLUBS EVERY MONDAY (term time only) 6-8pm
                   </Typography>
                   <Typography textAlign={"center"} color={"white"}>
@@ -95,37 +96,8 @@ const FilmClub = () => {
             </Card>
           )}
         </Grid>
-
-        <Grid item xs={12} md={6}>
-          <FilmClubDisplayCard
-            img={
-              "//img1.wsimg.com/isteam/ip/c75f83f5-5376-471b-af2d-7c3435beb175/36017984913_a367a47e19_b.jpg/:/cr=t:7.16%25,l:0%25,w:100%25,h:85.68%25/rs=w:600,h:300,cg:true"
-            }
-            heading={"ARE YOU INTERESTED IN MAKING WILDLIFE/NATURE FILMS?"}
-            description={"No"}
-          />
-        </Grid>
-
-        <Grid item xs={12} md={6}>
-          <FilmClubDisplayCard
-            img={
-              "//img1.wsimg.com/isteam/ip/c75f83f5-5376-471b-af2d-7c3435beb175/2-275845-Main-600x450-7.jpg/:/cr=t:16.67%25,l:0%25,w:100%25,h:66.67%25/rs=w:600,h:300,cg:true"
-            }
-            heading={"A fun experience with filmmakers"}
-            description={
-              "Our award-winning film mentors are from professional arts and media backgrounds. We also invite guest speakers from the world of film and television. Guest speakers have included OSCAR and BAFTA winning Actors, Directors, Producers Propmakes, Make Up Artists and Screenwriters."
-            }
-          />
-        </Grid>
-
-        <Grid item xs={12} md={6}>
-          <FilmClubDisplayCard
-            img={
-              "//img1.wsimg.com/isteam/ip/c75f83f5-5376-471b-af2d-7c3435beb175/ff5ef498a6974abbf9f8cbb269fac47a628a60a5.jpg/:/rs=w:600,h:300,cg:true,m/cr=w:600,h:300"
-            }
-            heading={" We offer a range of film clubs"}
-            description={"Girl's Film Club (ages 13-19)"}
-          />
+        <Grid item xs={12}>
+          <FilmClubDisplayGrid />
         </Grid>
       </Grid>
     </>
