0829a6a58374327b57aa7b24d569c4bd4ef2287d
Nicholas Ameyaw Abuaku <abuaku1400@gmail.com>
2024-02-16 14:37:28 +0000

    removed destructuring props

--- kent-film-foundation/src/components/FilmClubDisplayCard.jsx
+++ kent-film-foundation/src/components/FilmClubDisplayCard.jsx
@@ -9,52 +9,49 @@ import {
   useMediaQuery,
 } from "@mui/material";
 export const FilmClubDisplayCard = (props) => {
-  const { img, heading, description } = props;
   const theme = useTheme();
   const isMobile = useMediaQuery(theme.breakpoints.down("md"));
   return (
     <>
       {isMobile ? (
-        <Card sx={{ width: "90%", height: "auto", marginLeft: "4%" }}>
-          <CardMedia component={"img"} height={"30%"} src={img} />
+        <Card
+          sx={{
+            width: "100%",
+            height: "auto",
+            marginLeft: "4%",
+            marginBottom: "4px",
+          }}
+          elevation={12}
+        >
+          <CardMedia component={"img"} height={"30%"} src={props.img} />
           <CardContent>
             <Stack spacing={2}>
-              <Typography
-                textAlign={"center"}
-                variant="subtitle1"
-                fontWeight={"bold"}
-              >
-                {heading}
+              <Typography textAlign={"center"} variant="h6" fontWeight={"bold"}>
+                {props.heading}
               </Typography>
-              <Typography textAlign={"center"} variant="body1">
-                {description}
+              <Typography textAlign={"center"} variant="subtitle1">
+                {props.description}
               </Typography>
             </Stack>
           </CardContent>
         </Card>
       ) : (
         <Card
-          width={"100%"}
-          height={"100%"}
+          elevation={12}
           sx={{
             width: "422px",
-            height: "50%",
             marginLeft: "0.875rem",
             marginTop: "40px",
           }}
         >
-          <CardMedia component={"img"} height={"300px"} src={img} />
+          <CardMedia component={"img"} height={"300px"} src={props.img} />
           <CardContent>
             <Stack spacing={2}>
-              <Typography
-                textAlign={"center"}
-                variant="subtitle1"
-                fontWeight={"bold"}
-              >
-                {heading}
+              <Typography textAlign={"center"} variant="h6" fontWeight={"bold"}>
+                {props.heading}
               </Typography>
-              <Typography textAlign={"center"} variant="subtitle2">
-                {description}
+              <Typography textAlign={"center"} variant="subtitle1">
+                {props.description}
               </Typography>
             </Stack>
           </CardContent>
