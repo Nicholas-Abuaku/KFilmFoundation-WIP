db2fa86591cf2b25bd9cb652c86fe29d0f54e49f
Nicholas Ameyaw Abuaku <abuaku1400@gmail.com>
2024-02-16 13:42:16 +0000

    Film Club page now uses film club display cards

--- kent-film-foundation/src/pages/FlimClub.jsx
+++ kent-film-foundation/src/pages/FlimClub.jsx
@@ -15,30 +15,11 @@ import {
   useThemeProps,
   createTheme,
 } from "@mui/material";
+import { FilmClubDisplayCard } from "../components/FilmClubDisplayCard";
 
 const FilmClub = () => {
   const theme = useTheme();
   const isMobile = useMediaQuery(theme.breakpoints.down("md"));
-
-  // theme.typography.h5 = {
-  //   fontSize: "1.2rem",
-  //   "@media (min-width:600px)": {
-  //     fontSize: "1.5rem",
-  //   },
-  //   [theme.breakpoints.up("md")]: {
-  //     fontSize: "1.5rem",
-  //   },
-  // };
-
-  // theme.typography.h6 = {
-  //   fontSize: "1.2rem",
-  //   "@media (min-width:600px)": {
-  //     fontSize: "1.5rem",
-  //   },
-  //   [theme.breakpoints.up("md")]: {
-  //     fontSize: "1.5rem",
-  //   },
-  // };
 
   return (
     <>
@@ -48,11 +29,19 @@ const FilmClub = () => {
         justifyContent={"center"}
         alignItems={"center"}
       >
-        <Grid item xs={12} md={12}>
+        <Grid
+          item
+          xs={12}
+          md={12}
+          justifyContent={"center"}
+          alignItems={"center"}
+        >
           {isMobile ? (
-            <Paper
-              elevation={24}
+            <Card
+              width={"64.16vw"}
+              height={"65.18vh"}
               sx={{
+                backgroundColor: "#88A79A",
                 width: "90%",
                 height: "100%",
                 margin: "auto",
@@ -60,234 +49,83 @@ const FilmClub = () => {
                 marginBottom: "80px",
               }}
             >
-              <Card
-                width={"64.16vw"}
-                height={"65.18vh"}
-                sx={{ backgroundColor: "#88A79A" }}
-              >
-                <CardMedia
-                  component={"img"}
-                  height={"100%"}
-                  src="//img1.wsimg.com/isteam/ip/c75f83f5-5376-471b-af2d-7c3435beb175/Girls%20film%20club%201.jpg/:/cr=t:16.67%25,l:0%25,w:100%25,h:66.67%25/rs=w:1240,h:620,cg:true"
-                />
-                <CardContent>
-                  <Stack spacing={2}>
-                    <Typography
-                      textAlign={"center"}
-                      variant="h5"
-                      color={"white"}
-                    >
-                      FREE YOUTH FILM CLUBS EVERY MONDAY (term time only) 6-8pm
-                    </Typography>
-                    <Typography textAlign={"center"} color={"white"}>
-                      For more information contact: kentfilmfoundation@gmail.com{" "}
-                    </Typography>
-                  </Stack>
-                </CardContent>
-              </Card>
-            </Paper>
+              <CardMedia
+                component={"img"}
+                height={"100%"}
+                src="//img1.wsimg.com/isteam/ip/c75f83f5-5376-471b-af2d-7c3435beb175/Girls%20film%20club%201.jpg/:/cr=t:16.67%25,l:0%25,w:100%25,h:66.67%25/rs=w:1240,h:620,cg:true"
+              />
+              <CardContent>
+                <Stack spacing={2}>
+                  <Typography textAlign={"center"} variant="h5" color={"white"}>
+                    FREE YOUTH FILM CLUBS EVERY MONDAY (term time only) 6-8pm
+                  </Typography>
+                  <Typography textAlign={"center"} color={"white"}>
+                    For more information contact: kentfilmfoundation@gmail.com{" "}
+                  </Typography>
+                </Stack>
+              </CardContent>
+            </Card>
           ) : (
-            <Paper
-              elevation={24}
+            <Card
+              height={"65.18vh"}
               sx={{
-                width: "64.16vw",
-                height: "65.18vh",
+                backgroundColor: "#88A79A",
                 margin: "auto",
                 marginTop: "40px",
                 marginBottom: "80px",
+                maxWidth: "80%",
               }}
+              elevation={24}
             >
-              <Card
-                width={"64.16vw"}
-                height={"65.18vh"}
-                sx={{ backgroundColor: "#88A79A" }}
-              >
-                <CardMedia
-                  component={"img"}
-                  height={"100%"}
-                  src="//img1.wsimg.com/isteam/ip/c75f83f5-5376-471b-af2d-7c3435beb175/Girls%20film%20club%201.jpg/:/cr=t:16.67%25,l:0%25,w:100%25,h:66.67%25/rs=w:1240,h:620,cg:true"
-                />
-                <CardContent>
-                  <Stack spacing={2}>
-                    <Typography
-                      textAlign={"center"}
-                      variant="h5"
-                      color={"white"}
-                    >
-                      FREE YOUTH FILM CLUBS EVERY MONDAY (term time only) 6-8pm
-                    </Typography>
-                    <Typography textAlign={"center"} color={"white"}>
-                      For more information contact: kentfilmfoundation@gmail.com{" "}
-                    </Typography>
-                  </Stack>
-                </CardContent>
-              </Card>
-            </Paper>
-          )}
-        </Grid>
-
-        <Grid item xs={12} md={3}>
-          <Paper
-            elevation={4}
-            sx={{
-              width: "90%",
-              height: "50%",
-              marginLeft: "0.875rem",
-              marginTop: "40px",
-            }}
-          >
-            <Card width={"100%"} height={"100%"}>
               <CardMedia
                 component={"img"}
-                height={"300px"}
-                src="//img1.wsimg.com/isteam/ip/c75f83f5-5376-471b-af2d-7c3435beb175/36017984913_a367a47e19_b.jpg/:/cr=t:7.16%25,l:0%25,w:100%25,h:85.68%25/rs=w:600,h:300,cg:true"
+                height={"50%"}
+                src="//img1.wsimg.com/isteam/ip/c75f83f5-5376-471b-af2d-7c3435beb175/Girls%20film%20club%201.jpg/:/cr=t:16.67%25,l:0%25,w:100%25,h:66.67%25/rs=w:1240,h:620,cg:true"
               />
               <CardContent>
                 <Stack spacing={2}>
-                  <Typography
-                    textAlign={"center"}
-                    variant="subtitle1"
-                    fontWeight={"bold"}
-                  >
-                    ARE YOU INTERESTED IN MAKING WILDLIFE/NATURE FILMS?
+                  <Typography textAlign={"center"} variant="h5" color={"white"}>
+                    FREE YOUTH FILM CLUBS EVERY MONDAY (term time only) 6-8pm
                   </Typography>
-                  <Typography textAlign={"center"} variant="subtitle2">
-                    BRAND NEW FOR THIS YEAR Free course for young adults 16 - 25
-                    years olds For more information contact:
-                  </Typography>
-                  <Typography textAlign={"center"} variant="subtitle2">
-                    {" "}
-                    kentfilmfoundation@gmail.com{" "}
+                  <Typography textAlign={"center"} color={"white"}>
+                    For more information contact: kentfilmfoundation@gmail.com{" "}
                   </Typography>
                 </Stack>
               </CardContent>
             </Card>
-          </Paper>
-        </Grid>
-
-        <Grid item xs={12} md={3}>
-          <Paper
-            elevation={4}
-            sx={{
-              width: "90%",
-              height: "50%",
-              marginLeft: "0.875rem",
-              marginTop: "100px",
-              marginBottom: "40px",
-            }}
-          >
-            <Card width={"100%"} height={"100%"}>
-              <CardMedia
-                component={"img"}
-                height={"300px"}
-                src="//img1.wsimg.com/isteam/ip/c75f83f5-5376-471b-af2d-7c3435beb175/2-275845-Main-600x450-7.jpg/:/cr=t:16.67%25,l:0%25,w:100%25,h:66.67%25/rs=w:600,h:300,cg:true"
-              />
-              <CardContent>
-                <Stack spacing={2}>
-                  <Typography
-                    textAlign={"center"}
-                    fontWeight={"bold"}
-                    variant="subtitle1"
-                  >
-                    A fun experience with filmmakers
-                  </Typography>
-                  <Typography textAlign={"center"} variant="subtitle2">
-                    Our award-winning film mentors are from professional arts
-                    and media backgrounds. We also invite guest speakers from
-                    the world of film and television. Guest speakers have
-                    included OSCAR and BAFTA winning Actors, Directors,
-                    Producer,s Propmakes, Make Up Artists and Screenwriters.
-                  </Typography>
-                </Stack>
-              </CardContent>
-            </Card>
-          </Paper>
-        </Grid>
-
-        <Grid item xs={12} md={3}>
-          {isMobile ? (
-            <Paper
-              elevation={4}
-              sx={{
-                width: "90%",
-                height: "100%",
-                marginTop: "100px",
-                marginBottom: "40px",
-                marginLeft: "0.875rem",
-              }}
-            >
-              <Card width={"100%"} height={"100%"}>
-                <CardMedia
-                  component={"img"}
-                  height={"300px"}
-                  src="//img1.wsimg.com/isteam/ip/c75f83f5-5376-471b-af2d-7c3435beb175/ff5ef498a6974abbf9f8cbb269fac47a628a60a5.jpg/:/rs=w:600,h:300,cg:true,m/cr=w:600,h:300"
-                />
-                <CardContent>
-                  <Stack spacing={0}>
-                    <Typography textAlign={"center"} fontWeight={"bold"}>
-                      We offer a range of film clubs
-                    </Typography>
-                    <Typography textAlign={"center"}>
-                      Girl's Film Club (ages 13-19)
-                    </Typography>
-                    <Typography textAlign={"center"}>
-                      New Talent Film Club (ages 14-19)
-                    </Typography>
-                    <Typography textAlign={"center"}>
-                      LGBT Film Club (ages 15-19)
-                    </Typography>
-                    <Typography textAlign={"center"}>
-                      Wildlife Cinematographer Programme is for young adults 16
-                      - 25
-                    </Typography>
-                  </Stack>
-                </CardContent>
-              </Card>
-            </Paper>
-          ) : (
-            <Paper
-              elevation={4}
-              sx={{
-                width: "100%",
-                height: "100%",
-                marginTop: "100px",
-                marginBottom: "40px",
-                marginLeft: "0.875rem",
-              }}
-            >
-              <Card width={"100%"} height={"100%"}>
-                <CardMedia
-                  component={"img"}
-                  height={"300px"}
-                  src="//img1.wsimg.com/isteam/ip/c75f83f5-5376-471b-af2d-7c3435beb175/ff5ef498a6974abbf9f8cbb269fac47a628a60a5.jpg/:/rs=w:600,h:300,cg:true,m/cr=w:600,h:300"
-                />
-                <CardContent>
-                  <Stack spacing={0}>
-                    <Typography
-                      textAlign={"center"}
-                      fontWeight={"bold"}
-                      variant="subtitle1"
-                    >
-                      We offer a range of film clubs
-                    </Typography>
-                    <Typography textAlign={"center"} variant="subtitle2">
-                      Girl's Film Club (ages 13-19)
-                    </Typography>
-                    <Typography textAlign={"center"} variant="subtitle2">
-                      New Talent Film Club (ages 14-19)
-                    </Typography>
-                    <Typography textAlign={"center"} variant="subtitle2">
-                      LGBT Film Club (ages 15-19)
-                    </Typography>
-                    <Typography textAlign={"center"} variant="subtitle2">
-                      Wildlife Cinematographer Programme is for young adults 16
-                      - 25
-                    </Typography>
-                  </Stack>
-                </CardContent>
-              </Card>
-            </Paper>
           )}
+        </Grid>
+
+        <Grid item xs={12} md={6}>
+          <FilmClubDisplayCard
+            img={
+              "//img1.wsimg.com/isteam/ip/c75f83f5-5376-471b-af2d-7c3435beb175/36017984913_a367a47e19_b.jpg/:/cr=t:7.16%25,l:0%25,w:100%25,h:85.68%25/rs=w:600,h:300,cg:true"
+            }
+            heading={"ARE YOU INTERESTED IN MAKING WILDLIFE/NATURE FILMS?"}
+            description={"No"}
+          />
+        </Grid>
+
+        <Grid item xs={12} md={6}>
+          <FilmClubDisplayCard
+            img={
+              "//img1.wsimg.com/isteam/ip/c75f83f5-5376-471b-af2d-7c3435beb175/2-275845-Main-600x450-7.jpg/:/cr=t:16.67%25,l:0%25,w:100%25,h:66.67%25/rs=w:600,h:300,cg:true"
+            }
+            heading={"A fun experience with filmmakers"}
+            description={
+              "Our award-winning film mentors are from professional arts and media backgrounds. We also invite guest speakers from the world of film and television. Guest speakers have included OSCAR and BAFTA winning Actors, Directors, Producers Propmakes, Make Up Artists and Screenwriters."
+            }
+          />
+        </Grid>
+
+        <Grid item xs={12} md={6}>
+          <FilmClubDisplayCard
+            img={
+              "//img1.wsimg.com/isteam/ip/c75f83f5-5376-471b-af2d-7c3435beb175/ff5ef498a6974abbf9f8cbb269fac47a628a60a5.jpg/:/rs=w:600,h:300,cg:true,m/cr=w:600,h:300"
+            }
+            heading={" We offer a range of film clubs"}
+            description={"Girl's Film Club (ages 13-19)"}
+          />
         </Grid>
       </Grid>
     </>
