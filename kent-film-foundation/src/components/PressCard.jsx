36894931433edefea8ca752f30752961b3eed3dc
Nicholas Ameyaw Abuaku <abuaku1400@gmail.com>
2024-02-16 10:45:58 +0000

    Now has separate mobile layout

--- kent-film-foundation/src/components/PressCard.jsx
+++ kent-film-foundation/src/components/PressCard.jsx
@@ -7,58 +7,120 @@ import {
   Typography,
   Button,
   Link,
-  Skeleton,
+  useTheme,
+  useMediaQuery,
 } from "@mui/material";
 import React from "react";
 
 export const PressCard = (props) => {
   const { newsSource, articleTitle, articleUrl, image } = props;
+  const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.down("md"));
   return (
-    <Card
-      width={"100%"}
-      sx={{
-        backgroundColor: "#339465",
-        minHeight: "462px",
-        width: "369.5px",
-        color: "white",
-        marginLeft: "10px",
-        marginBottom: "30px",
-        borderRadius: "0px",
-      }}
-    >
-      <CardMedia
-        component={"img"}
-        height={"300px"}
-        src={image}
-        loading="eager"
-        title={props.articleTitle}
-        alt={"News coverage from " + props.newsSource}
-      />
-      <CardContent>
-        <Stack spacing={0}>
-          <Typography
-            textAlign={"center"}
-            fontWeight={"bold"}
-            variant="subtitle1"
-            component={"h3"}
-          >
-            {newsSource}
-          </Typography>
-          <Typography textAlign={"center"} variant="body1">
-            {articleTitle}
-          </Typography>
-        </Stack>
-        <Stack alignItems={"center"} justifyContent={"center"} marginTop={2}>
-          <Button
-            variant="outlined"
-            color="inherit"
-            component={Link}
-            to={articleUrl}
-          >
-            Learn More
-          </Button>
-        </Stack>
-      </CardContent>
-    </Card>
+    <>
+      {isMobile ? (
+        <Card
+          width={"100%"}
+          sx={{
+            backgroundColor: "#339465",
+            height: "auto",
+            width: "90%",
+            color: "white",
+            marginLeft: "10px",
+            marginBottom: "30px",
+            borderRadius: "0px",
+          }}
+        >
+          <CardMedia
+            component={"img"}
+            height={"300px"}
+            src={image}
+            loading="eager"
+            title={props.articleTitle}
+            alt={"News coverage from " + props.newsSource}
+          />
+          <CardContent>
+            <Stack spacing={0}>
+              <Typography
+                textAlign={"center"}
+                fontWeight={"bold"}
+                variant="subtitle1"
+                component={"h3"}
+              >
+                {newsSource}
+              </Typography>
+              <Typography textAlign={"center"} variant="body1">
+                {articleTitle}
+              </Typography>
+            </Stack>
+            <Stack
+              alignItems={"center"}
+              justifyContent={"center"}
+              marginTop={2}
+            >
+              <Button
+                variant="outlined"
+                color="inherit"
+                component={Link}
+                to={articleUrl}
+              >
+                Learn More
+              </Button>
+            </Stack>
+          </CardContent>
+        </Card>
+      ) : (
+        <Card
+          width={"100%"}
+          sx={{
+            backgroundColor: "#339465",
+            minHeight: "462px",
+            width: "369.5px",
+            color: "white",
+            marginLeft: "10px",
+            marginBottom: "30px",
+            borderRadius: "0px",
+          }}
+        >
+          <CardMedia
+            component={"img"}
+            height={"300px"}
+            src={image}
+            loading="eager"
+            title={props.articleTitle}
+            alt={"News coverage from " + props.newsSource}
+          />
+          <CardContent>
+            <Stack spacing={0}>
+              <Typography
+                textAlign={"center"}
+                fontWeight={"bold"}
+                variant="subtitle1"
+                component={"h3"}
+              >
+                {newsSource}
+              </Typography>
+              <Typography textAlign={"center"} variant="body1">
+                {articleTitle}
+              </Typography>
+            </Stack>
+            <Stack
+              alignItems={"center"}
+              justifyContent={"center"}
+              marginTop={2}
+            >
+              <Button
+                variant="outlined"
+                color="inherit"
+                component={Link}
+                to={articleUrl}
+              >
+                Learn More
+              </Button>
+            </Stack>
+          </CardContent>
+        </Card>
+      )}
+    </>
   );
 };
