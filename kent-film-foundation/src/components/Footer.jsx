00d18e45b72246409fdd93abcfc2e7057a0a06bf
Nicholas Ameyaw Abuaku <abuaku1400@gmail.com>
2024-02-16 10:45:25 +0000

    Changed mobile footer font size to be smaller

--- kent-film-foundation/src/components/Footer.jsx
+++ kent-film-foundation/src/components/Footer.jsx
@@ -12,16 +12,7 @@ export const Footer = () => {
   const theme = useTheme();
   const isMobile = useMediaQuery(theme.breakpoints.down("md"));
   const textTheme = createTheme();
-  textTheme.typography.h6 = {
-    fontSize: "0.1 rem",
-    fontFamily: "Open Sans, arial, sans-serif",
-    "@media (min-width:600px)": {
-      fontSize: "1rem",
-    },
-    [theme.breakpoints.up("md")]: {
-      fontSize: "1rem",
-    },
-  };
+
   return (
     <Grid
       container
@@ -33,11 +24,15 @@ export const Footer = () => {
         <SocialMediaLinks />
       </Grid>
       <Grid item xs={12}>
-        <ThemeProvider theme={textTheme}>
+        {isMobile ? (
+          <Typography textAlign={"center"} variant="caption" component={"h4"}>
+            COPYRIGHT © 2024 KENT FILM FOUNDATION - ALL RIGHTS RESERVED.
+          </Typography>
+        ) : (
           <Typography variant="h6" textAlign={"center"} component={"h4"}>
             COPYRIGHT © 2024 KENT FILM FOUNDATION - ALL RIGHTS RESERVED.
           </Typography>
-        </ThemeProvider>
+        )}
       </Grid>
     </Grid>
   );
