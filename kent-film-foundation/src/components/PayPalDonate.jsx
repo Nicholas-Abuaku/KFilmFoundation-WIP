39e0e238564a67440d84efef98f79f38ed46509a
Nicholas Ameyaw Abuaku <abuaku1400@gmail.com>
2024-02-17 14:01:55 +0000

    Paypal donate button

---
+++ kent-film-foundation/src/components/PayPalDonate.jsx
@@ -0,0 +1,16 @@
+import { Button, IconButton } from "@mui/material";
+import React from "react";
+import { Link } from "react-router-dom";
+import PaidIcon from "@mui/icons-material/Paid";
+
+export const PayPalDonate = () => {
+  return (
+    <Button
+      endIcon={<PaidIcon />}
+      variant="contained"
+      sx={{ borderRadius: "20px", backgroundColor: "#339465" }}
+    >
+      Donate
+    </Button>
+  );
+};
