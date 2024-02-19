cdd600bcf6f8d792cf18187a4b0931057a74a691
Nicholas Ameyaw Abuaku <abuaku1400@gmail.com>
2024-02-19 12:14:46 +0000

    Changed button grid to include exit button

--- kent-film-foundation/src/components/back-end/ButtonGrid.jsx
+++ kent-film-foundation/src/components/back-end/ButtonGrid.jsx
@@ -1,8 +1,9 @@
-import React from 'react'
-import { Grid, Stack, Paper } from '@mui/material';
-import { DashboardFeaturedButton } from './DashboardFeaturedButton';
-import { DashboardManageButton } from './DashboardManageButton';
-import { DashboardManageNewsButton } from './DashboardManageNewsButton';
+import React from "react";
+import { Grid, Stack, Paper } from "@mui/material";
+import { DashboardFeaturedButton } from "./DashboardFeaturedButton";
+import { DashboardManageButton } from "./DashboardManageButton";
+import { DashboardManageNewsButton } from "./DashboardManageNewsButton";
+import { DashboardSignOutButton } from "./DashboardSignOutButton";
 export const ButtonGrid = () => {
   return (
     // <Stack direction={'row-reverse'} spacing={2} marginTop={3}>
@@ -10,19 +11,26 @@ export const ButtonGrid = () => {
     //     <DashboardManageButton/>
     //     <DashboardManageNewsButton/>
     // </Stack>
-    <Grid container spacing={2} justifyContent={'center'} alignItems={'center'} marginTop={'30%'} marginLeft={'120%'}>
+    <Grid
+      container
+      spacing={2}
+      justifyContent={"center"}
+      alignItems={"center"}
+      marginTop={"30%"}
+      marginLeft={"120%"}
+    >
       <Grid item xs={8}>
-        <DashboardFeaturedButton/>
+        <DashboardFeaturedButton />
       </Grid>
       <Grid item xs={4}>
-        <DashboardManageButton/>
+        <DashboardManageButton />
       </Grid>
       <Grid item xs={8}>
-        <DashboardManageNewsButton/>
+        <DashboardManageNewsButton />
       </Grid>
       <Grid item xs={4}>
-        <DashboardManageNewsButton/>
+        <DashboardSignOutButton />
       </Grid>
     </Grid>
-  )
-}
+  );
+};
