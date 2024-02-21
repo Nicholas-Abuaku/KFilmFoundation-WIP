620055c4cca60069845ef09340de9368596e7995
Nicholas Ameyaw Abuaku <abuaku1400@gmail.com>
2024-02-21 17:31:43 +0000

    using PressTheme

--- kent-film-foundation/src/pages/Press.jsx
+++ kent-film-foundation/src/pages/Press.jsx
@@ -1,11 +1,18 @@
-import { Grid, Typography, useTheme, useMediaQuery } from "@mui/material";
+import {
+  Grid,
+  Typography,
+  useTheme,
+  useMediaQuery,
+  ThemeProvider,
+  createTheme,
+} from "@mui/material";
 import { PressCardGrid } from "../components/back-end/PressCardGrid";
 import { Helmet } from "react-helmet-async";
+import PressTheme from "../Theme/PressTheme";
 const Press = () => {
-  const theme = useTheme();
-  const isMobile = useMediaQuery(theme.breakpoints.down("md"));
+  const isMobile = useMediaQuery(PressTheme.breakpoints.down("md"));
   return (
-    <>
+    <ThemeProvider theme={createTheme(PressTheme)}>
       <Helmet>
         <title>Press</title>
         <meta
@@ -15,21 +22,16 @@ const Press = () => {
         <link rel="canonical" href="http://localhost:5173/press" />
       </Helmet>
       <Grid container justifyContent={"center"} alignItems={"center"}>
-        <Grid item md={12} xs={12}>
-          <Typography
-            variant="h3"
-            textAlign={"center"}
-            component={"h2"}
-            sx={{ textDecoration: "underline" }}
-          >
-            Press
+        <Grid item md={12} xs={12} marginBottom={4}>
+          <Typography variant="h3" textAlign={"center"} component={"h2"}>
+            Kent Film Foundation in the Press
           </Typography>
         </Grid>
         <Grid>
           <PressCardGrid />
         </Grid>
       </Grid>
-    </>
+    </ThemeProvider>
   );
 };
 
