8441afad14f0f7753af23801317de4c07fcb1b32
Nicholas Ameyaw Abuaku <abuaku1400@gmail.com>
2024-02-21 17:32:30 +0000

    Created contact theme

---
+++ kent-film-foundation/src/Theme/ContactTheme.jsx
@@ -0,0 +1,10 @@
+import { createTheme, responsiveFontSizes } from "@mui/material/styles";
+
+let ContactTheme = createTheme({
+  typography: {
+    h6: {},
+  },
+});
+
+ContactTheme = responsiveFontSizes(ContactTheme);
+export default ContactTheme;
