50c950c41ce4e9776aa616295bcc5bdbc1f8ccc0
Nicholas Ameyaw Abuaku <abuaku1400@gmail.com>
2024-02-23 12:07:58 +0000

    Added pagination

--- kent-film-foundation/src/components/back-end/PressCardGrid.jsx
+++ kent-film-foundation/src/components/back-end/PressCardGrid.jsx
@@ -1,10 +1,13 @@
-import { Grid, Skeleton } from "@mui/material";
+import { Grid, Skeleton, Pagination } from "@mui/material";
+import { PressCard2 } from "../PressCard2";
 import { PressCard } from "../PressCard";
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 export const PressCardGrid = () => {
   const [pressData, setPressData] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [page, setPage] = useState(1);
+  const itemsPerPage = 3;
   const fetchPressData = async () => {
     try {
       axios.get("https://kentfilm.up.railway.app/api/press").then((res) => {
@@ -17,34 +20,47 @@ export const PressCardGrid = () => {
     }
   };
 
+  const handlePageChange = (event, newPage) => {
+    setPage(newPage);
+  };
+  const startIndex = (page - 1) * itemsPerPage;
+  const endIndex = startIndex + itemsPerPage;
   useEffect(() => {
     fetchPressData();
   }, []);
   return (
-    <Grid container spacing={2}>
-      {isLoading
-        ? pressData.map((article) => (
-            <Grid item md={5} xs={12} xl={3} key={article.id}>
-              <PressCard
-                newsSource={article.news_source}
-                articleTitle={article.article_title}
-                articleUrl={article.article_url}
-                image={
-                  "https://kentfilm.up.railway.app/storage/" + article.image
-                }
-              />
-            </Grid>
-          ))
-        : Array.from({ length: 4 }).map((_, index) => (
-            <Grid item key={index} md={5} xs={12} xl={3}>
-              <Skeleton
-                variant="rect"
-                width={369.5}
-                height={462}
-                animation="wave"
-              />
-            </Grid>
-          ))}
-    </Grid>
+    <>
+      <Grid container spacing={5} sx={{ margin: "auto" }} maxWidth={"100%"}>
+        {isLoading
+          ? pressData.slice(startIndex, endIndex).map((article) => (
+              <Grid item md={5} xs={12} xl={4} key={article.id}>
+                <PressCard
+                  newsSource={article.news_source}
+                  articleTitle={article.article_title}
+                  articleUrl={article.article_url}
+                  image={
+                    "https://kentfilm.up.railway.app/storage/" + article.image
+                  }
+                />
+              </Grid>
+            ))
+          : Array.from({ length: 4 }).map((_, index) => (
+              <Grid item key={index} md={5} xs={12} xl={4}>
+                <Skeleton
+                  variant="rect"
+                  width={550}
+                  height={462}
+                  animation="wave"
+                />
+              </Grid>
+            ))}
+      </Grid>
+      <Pagination
+        count={Math.ceil(pressData.length / itemsPerPage)}
+        page={page}
+        onChange={handlePageChange}
+        sx={{ mt: 2, justifyContent: "center" }}
+      />
+    </>
   );
 };
