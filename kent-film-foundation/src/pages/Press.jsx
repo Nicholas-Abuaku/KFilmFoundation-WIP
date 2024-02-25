4cccc1883e40d6dbb5db130d5382518ee79fc0ad
Nicholas Ameyaw Abuaku <abuaku1400@gmail.com>
2024-02-25 17:54:26 +0000

    Changed heading and font colours

--- kent-film-foundation/src/pages/Press.jsx
+++ kent-film-foundation/src/pages/Press.jsx
@@ -26,7 +26,10 @@ const Press = () => {
       <Grid container justifyContent={"center"} alignItems={"center"}>
         <Grid item md={12} xs={12} marginBottom={0}>
           <Typography variant="h3" textAlign={"center"} component={"h2"}>
-            Kent Film Foundation in the Press
+            Media & Press
+          </Typography>
+          <Typography variant="h6" textAlign={"center"} component={"h2"}>
+            Kent Film Foundation Press and Media Coverage
           </Typography>
         </Grid>
         <Grid item xs={12}>
